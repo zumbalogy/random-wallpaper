e4cec085c817254d99a2ee53a46ab3de4a4c15c1
zumbalogy <intrapostal@gmail.com>
2016-02-23 12:40:17 -0500

    now does two words

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 var http = require('http');
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { console.log(stdout) }
+var child_process = require('child_process');
+var exec = child_process.exec;
 
 var img_file = "/tmp/script-wallpaper.jpg";
-
-var options = {
-  host: 'images.search.yahoo.com',
-  path: '/search/images?p=bears+with+beaks'
-}
 
 function callback(response) {
   var str = '';
@@ -17,18 +12,38 @@ function callback(response) {
   });
 
   response.on('end', function () {
-    var raw_urls = str.match(/imgurl=.+?&/g);
-    var clean_urls = raw_urls.map(function(url) {
-      return url.substring(7, url.length - 1);
-    });
-    var urls = clean_urls.map(decodeURIComponent);
-    var randPick = Math.floor((urls.length - 1) * Math.random());
-    var url = urls[randPick];
-    exec('curl ' + url + ' > ' + img_file, puts);
-    exec('gsettings set org.cinnamon.desktop.background picture-uri "file://' + img_file + '"', puts);
+    try {
+      var raw_urls = str.match(/imgurl=.+?&/g);
+      var clean_urls = raw_urls.map(function(url) {
+        return url.substring(7, url.length - 1);
+      });
+      var urls = clean_urls.map(decodeURIComponent);
+      var randPick = Math.floor((urls.length - 1) * Math.random());
+      var url = urls[randPick];
+      child_process.execSync('curl ' + url + ' > ' + img_file);
+      child_process.execSync('gsettings set org.cinnamon.desktop.background picture-uri "file://' + img_file + '"');
+    } catch (e) {
+      console.log('whoops ', e);
+    }
   });
 }
 
+function randomWord() {
+  var index = Math.floor(Math.random() * 19000);
+  var cmd = 'sed "' + index + 'q;d" words.txt';
+  var dirty_word = child_process.execSync(cmd, { encoding: 'utf8' });
+  var word = dirty_word.trim()
+  console.log(word);
+  return word;
+}
+
+child_process.execSync("gsettings set org.gnome.desktop.background picture-options 'centered'");
+
 setInterval(function() {
+  var host = 'images.search.yahoo.com';
+  var path = '/search/images?p=' + randomWord() + '+' + randomWord();
+  var options = {};
+  options.host = host;
+  options.path = path;
   http.request(options, callback).end();
-}, 5000)
+}, 7000)
